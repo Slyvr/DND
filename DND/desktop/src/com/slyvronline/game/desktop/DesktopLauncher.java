21f5f5a95c125bd5cab09a91685752f0931ef22a
Matthew Schrum <mschrum89@gmail.com>
2015-11-29 11:48:38 -0600

    Initial code and content commit

--- DND/desktop/src/com/slyvronline/game/desktop/DesktopLauncher.java
+++ DND/desktop/src/com/slyvronline/game/desktop/DesktopLauncher.java
@@ -3,10 +3,15 @@ package com.slyvronline.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.slyvronline.game.Game;
+import com.slyvronline.game.utils.GameConstants;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = Game.TITLE;
+		config.width = GameConstants.DEFAULT_WIDTH;
+		config.height = GameConstants.DEFAULT_HEIGHT;
+		
 		new LwjglApplication(new Game(), config);
 	}
 }
